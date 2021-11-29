a072e39df24a246abf38fc0e30461d95d4239dd9
Marponset <joel.marpons@mataro.epiaedu.cat>
2021-11-29 20:08:55 +0100

    Prova registre

--- app/src/main/java/com/example/verbumquest/MainActivity.java
+++ app/src/main/java/com/example/verbumquest/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.verbumquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button pito;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
