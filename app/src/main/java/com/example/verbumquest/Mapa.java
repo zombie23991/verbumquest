549c870e5d9ab09b226fd7fac803bae782f9dc08
Adrià Castany <adriacastany@hotmail.es>
2021-12-10 02:00:42 +0100

    mapa i seleccionador de nivells acabats

--- app/src/main/java/com/example/verbumquest/Mapa.java
+++ app/src/main/java/com/example/verbumquest/Mapa.java
@@ -1,7 +1,10 @@
 package com.example.verbumquest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.RecoverySystem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,12 +13,25 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Mapa extends AppCompatActivity {
 
+    Button mon1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
 
+        mon1 = findViewById(R.id.mon1);
+
+        mon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Mapa.this, MonPla.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
 
 }
---
+++ app/src/main/java/com/example/verbumquest/model/ItemList.java
@@ -0,0 +1,49 @@
+package com.example.verbumquest.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemList  {
+    private String titol;
+    private String npreguntes;
+    private int imgResource;
+    private int imgStarOne;
+    private int imgStarTwo;
+    private int imgStarThree;
+
+
+    //Creem els getters per extreure informació
+    //El ordre sera com es tindra que declarar al crear un nivell
+    public ItemList(String titol, String npreguntes,int imgResource, int imgStarOne, int imgStarTwo, int imgStarThree) {
+        this.titol = titol;
+        this.npreguntes = npreguntes;
+        this.imgResource = imgResource;
+        this.imgStarOne = imgStarOne;
+        this.imgStarTwo = imgStarTwo;
+        this.imgStarThree = imgStarThree;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public String getNpreguntes() {
+        return npreguntes;
+    }
+
+    public int getImgResource() {
+        return imgResource;
+    }
+
+    public int getImgStarOne() {
+        return imgStarOne;
+    }
+
+    public int getImgStarTwo() {
+        return imgStarTwo;
+    }
+
+    public int getImgStarThree() {
+        return imgStarThree;
+    }
+
+}
