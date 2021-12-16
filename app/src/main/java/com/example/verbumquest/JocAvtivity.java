f851550ee9b2f0a6afd39770ccf1730ff1d0f42a
Adrià Castany <adriacastany@hotmail.es>
2021-12-16 20:24:56 +0100

    funcions de vides enemics

--- app/src/main/java/com/example/verbumquest/JocAvtivity.java
+++ app/src/main/java/com/example/verbumquest/JocAvtivity.java
@@ -1,6 +1,5 @@
 package com.example.verbumquest;
 
-import android.content.ClipData;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,21 +8,17 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.example.verbumquest.adapter.RecyclerAdapter;
 import com.example.verbumquest.model.ItemList;
 import com.example.verbumquest.model.preguntes;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.Random;
 
 import pl.droidsonroids.gif.GifImageView;
 
 public class JocAvtivity extends AppCompatActivity {
+    private ImageView imgLiveOne, imgLiveTwo, imgLiveThree, imgEvilLive1, imgEvilLive2, imgEvilLive3, imgEvilLive4, imgEvilLive5;
     private GifImageView imgResource;
     private ConstraintLayout fons;
     private TextView mundo;
@@ -34,6 +29,7 @@ public class JocAvtivity extends AppCompatActivity {
     public jugador jugador = new jugador(3);
     public enemic enemic = new enemic(5);
     private int videsJugador , videsEnemic;
+
     Random random;
     int currentScore = 0, questionAttempted = 1, currentPos;
 
@@ -47,9 +43,23 @@ public class JocAvtivity extends AppCompatActivity {
         setTitle(getClass().getSimpleName());
         fons = findViewById(R.id.fons);
 
+        //vides del protagonista
+        imgLiveOne = findViewById(R.id.imgLiveOne);
+        imgLiveTwo = findViewById(R.id.imgLiveTwo);
+        imgLiveThree = findViewById(R.id.imgLiveThree);
+
+        //vides del enemic
+        imgEvilLive1 = findViewById(R.id.imgEvilLive1);
+        imgEvilLive2 = findViewById(R.id.imgEvilLive2);
+        imgEvilLive3 = findViewById(R.id.imgEvilLive3);
+        imgEvilLive4 = findViewById(R.id.imgEvilLive4);
+        imgEvilLive5 = findViewById(R.id.imgEvilLive5);
+
+        vidas();
         initViews();
         initValues();
         escenari();
+
         questionList = new ArrayList();
         tvQuestions = findViewById(R.id.pregunta);
         b1 = findViewById(R.id.resposta1);
@@ -68,6 +78,7 @@ public class JocAvtivity extends AppCompatActivity {
                     enemic.setVides(enemic.getVides() - 1 );
                     contador++;
                     if(enemic.getVides() >= 0 || contador <= 0) {
+                        restarvidasenemic();
                         currentPos = random.nextInt(questionList.size());
                         setDataToViews(currentPos);
                     }
@@ -77,9 +88,11 @@ public class JocAvtivity extends AppCompatActivity {
                 } else {
                     if(jugador.getVides() == 1) {
                         jugador.setVides(jugador.getVides() - 1);
+                        restarvidasprota();
                         finish();
                     } else {
                         jugador.setVides(jugador.getVides() - 1);
+                        restarvidasprota();
                     }
                 }
             }
@@ -92,6 +105,7 @@ public class JocAvtivity extends AppCompatActivity {
                     enemic.setVides(enemic.getVides() - 1 );
                     contador++;
                     if(enemic.getVides() >= 0 || contador < 0) {
+                        restarvidasenemic();
                         currentPos = random.nextInt(questionList.size());
                         setDataToViews(currentPos);
                     }
@@ -101,9 +115,11 @@ public class JocAvtivity extends AppCompatActivity {
                 } else {
                     if(jugador.getVides() == 1) {
                         jugador.setVides(jugador.getVides() - 1);
+                        restarvidasprota();
                         finish();
                     } else {
                         jugador.setVides(jugador.getVides() - 1);
+                        restarvidasprota();
                     }
                 }
             }
@@ -116,6 +132,7 @@ public class JocAvtivity extends AppCompatActivity {
                     enemic.setVides(enemic.getVides() - 1 );
                     contador++;
                     if(enemic.getVides() >= 0 || contador < 0) {
+                        restarvidasenemic();
                         currentPos = random.nextInt(questionList.size());
                         setDataToViews(currentPos);
                     }
@@ -125,9 +142,11 @@ public class JocAvtivity extends AppCompatActivity {
                 } else {
                     if(jugador.getVides() == 1) {
                         jugador.setVides(jugador.getVides() - 1);
+                        restarvidasprota();
                         finish();
                     } else {
                         jugador.setVides(jugador.getVides() - 1);
+                        restarvidasprota();
                     }
                 }
             }
@@ -141,6 +160,7 @@ public class JocAvtivity extends AppCompatActivity {
                     enemic.setVides(enemic.getVides() - 1 );
                     contador++;
                     if(enemic.getVides() >= 0 || contador < 0) {
+                        restarvidasenemic();
                         currentPos = random.nextInt(questionList.size());
                         setDataToViews(currentPos);
                     }
@@ -150,9 +170,11 @@ public class JocAvtivity extends AppCompatActivity {
                 } else {
                     if(jugador.getVides() == 1) {
                         jugador.setVides(jugador.getVides() - 1);
+                        restarvidasprota();
                         finish();
                     } else {
                         jugador.setVides(jugador.getVides() - 1);
+                        restarvidasprota();
                     }
                 }
             }
@@ -187,6 +209,49 @@ public class JocAvtivity extends AppCompatActivity {
         }else if(mundo.getText().equals("Torre")){
             fons.setBackgroundResource(R.drawable.torre);
         }
+    }
+
+    private void restarvidasprota(){
+
+        if(jugador.getVides() == 0){
+        imgLiveOne.setImageResource(R.drawable.heartoff);}
+        else if(jugador.getVides() == 1){
+        imgLiveTwo.setImageResource(R.drawable.heartoff);}
+        else if(jugador.getVides() == 2){
+        imgLiveThree.setImageResource(R.drawable.heartoff);}
+
+    }
+
+    private void restarvidasenemic(){
+
+        if(enemic.getVides() == 4) {
+            imgEvilLive5.setImageResource(R.drawable.heartoff);
+        }else if(enemic.getVides() == 3){
+            imgEvilLive4.setImageResource(R.drawable.heartoff);
+        }else if(enemic.getVides() == 2) {
+            imgEvilLive3.setImageResource(R.drawable.heartoff);
+        }else if(enemic.getVides() == 1) {
+            imgEvilLive2.setImageResource(R.drawable.heartoff);
+        }else if(enemic.getVides() == 0) {
+            imgEvilLive1.setImageResource(R.drawable.heartoff);
+        }
+
+    }
+
+    private void vidas(){
+        //Insertar vidas al inici del joc
+        //protagonista
+        imgLiveOne.setImageResource(R.drawable.hearton);
+        imgLiveTwo.setImageResource(R.drawable.hearton);
+        imgLiveThree.setImageResource(R.drawable.hearton);
+
+        //enemic
+        imgEvilLive1.setImageResource(R.drawable.heartevil);
+        imgEvilLive2.setImageResource(R.drawable.heartevil);
+        imgEvilLive3.setImageResource(R.drawable.heartevil);
+        imgEvilLive4.setImageResource(R.drawable.heartevil);
+        imgEvilLive5.setImageResource(R.drawable.heartevil);
+
     }
 
     private void initViews(){
